6392eda71b14a5d32dc670961ade9f385af36f0d
Sahil Tiwari <sahiltiwari.contactme@gmail.com>
2026-02-20 23:58:47 +0530

    refactor(ui): enhance home UI with action cards and icon-based layout

---
+++ src/components/PrimaryCard.tsx
@@ -0,0 +1,27 @@
+import { View, Text, Pressable, PressableProps } from "react-native";
+import React from "react";
+
+type PrimaryCardProps = {
+  text: string;
+  icon?: React.ComponentType<any>;
+} & PressableProps;
+
+export default function PrimaryCard({
+  text,
+  icon: Icon,
+
+  ...rest
+}: PrimaryCardProps) {
+  return (
+    <Pressable
+      className="w-48 h-36 justify-center items-center rounded-xl 
+        bg-[#1A1A1A] gap-3"
+      {...rest}
+    >
+      <View className="bg-[#0F0F0F] w-14 h-14 justify-center items-center rounded-full">
+        {Icon && <Icon width={18} height={18} fill="#ffffff" />}
+      </View>
+      <Text className="text-white text-lg">{text}</Text>
+    </Pressable>
+  );
+}
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,45 +1,62 @@
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import SettingsIcon from "../assets/icons/settings.svg";
+import ScannerIcon from "../assets/icons/qr-scan.svg";
+import NextIcon from "../assets/icons/next.svg";
+import QRIcon from "../assets/icons/qr.svg";
+import PrimaryCard from "../components/PrimaryCard";
 
 export default function HomeScreen() {
   const navigation = useNavigation<any>();
   return (
     <SafeAreaView>
-      <View>
-        <View className="bg-amber-600">
-          <Text>Linkly</Text>
+      <View className="px-5 h-full justify-between">
+        <View className="w-full flex-row items-center justify-between">
+          <Text className="text-white font-bold text-2xl">Linkly</Text>
           <Pressable
             onPress={() => {
               navigation.navigate("SettingsScreen");
             }}
           >
-            <Text>Settings</Text>
+            <SettingsIcon width={24} height={24} fill={"#ffffff"} />
           </Pressable>
         </View>
         <View>
           <Pressable
+            className="bg-[#4f8cff] p-3 flex-row items-center justify-between rounded-xl"
             onPress={() => {
               navigation.navigate("ScanQRScreen");
             }}
           >
-            <Text>Scan QR</Text>
-            <Text>Connect with new people</Text>
+            <View className="flex-row items-center gap-3">
+              <ScannerIcon width={24} height={24} fill={"#ffffff"} />
+              <View>
+                <Text className="text-white text-xl font-semibold">
+                  Scan QR
+                </Text>
+                <Text className="text-white">Connect with new people</Text>
+              </View>
+            </View>
+            <NextIcon width={24} height={24} fill={"#ffffff"} />
           </Pressable>
-          <Pressable
-            onPress={() => {
-              navigation.navigate("MyQRScreen");
-            }}
-          >
-            <Text>My Code</Text>
-          </Pressable>
-          <Pressable
-            onPress={() => {
-              navigation.navigate("SavedProfilesScreen");
-            }}
-          >
-            <Text>Saved</Text>
-          </Pressable>
+          <View className="flex-row justify-between items-center">
+            <PrimaryCard
+              icon={QRIcon}
+              text="My Code"
+              onPress={() => {
+                navigation.navigate("MyQRScreen");
+              }}
+            />
+
+            <PrimaryCard
+              icon={QRIcon}
+              text="Saved"
+              onPress={() => {
+                navigation.navigate("SavedProfilesScreen");
+              }}
+            />
+          </View>
         </View>
       </View>
     </SafeAreaView>
