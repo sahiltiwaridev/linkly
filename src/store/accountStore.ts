84ed94c3d77f19a58f74e03882e2f9147dfef8de
Sahil Tiwari <sahiltiwari.contactme@gmail.com>
2026-06-04 16:20:24 +0530

    refactor(store): add AccountStore type to useAccountStore for full type safety

--- src/store/accountStore.ts
+++ src/store/accountStore.ts
@@ -1,6 +1,27 @@
 import { create } from "zustand";
 import { Gender, UserData } from "../types/user.types";
 
+type AccountStore = UserData & {
+  setName: (name: string) => void;
+  setGender: (gender: Gender) => void;
+  setProfession: (profession: string) => void;
+  setBio: (bio: string) => void;
+  setPhone: (phone: string) => void;
+  setEmail: (email: string) => void;
+  setWhatsapp: (whatsapp: string) => void;
+  setLinkOneUrl: (linkOneUrl: string) => void;
+  setLinkTwoUrl: (linkTwoUrl: string) => void;
+  setLinkThreeUrl: (linkThreeUrl: string) => void;
+  setLinkFourUrl: (linkFourUrl: string) => void;
+  setLinkFiveUrl: (linkFiveUrl: string) => void;
+  setLinkOneTitle: (linkOneTitle: string) => void;
+  setLinkTwoTitle: (linkTwoTitle: string) => void;
+  setLinkThreeTitle: (linkThreeTitle: string) => void;
+  setLinkFourTitle: (linkFourTitle: string) => void;
+  setLinkFiveTitle: (linkFiveTitle: string) => void;
+  resetAccount: () => void;
+};
+
 const INITIAL_STATE: UserData = {
   name: "",
   gender: "neutral" as Gender,
@@ -21,28 +42,28 @@ const INITIAL_STATE: UserData = {
   linkFiveTitle: "",
 };
 
-export const useAccountStore = create((set) => ({
+export const useAccountStore = create<AccountStore>((set) => ({
   ...INITIAL_STATE,
 
-  setName: (name: string) => set({ name }),
-  setGender: (gender: Gender) => set({ gender }),
-  setProfession: (profession: string) => set({ profession }),
-  setBio: (bio: string) => set({ bio }),
-  setPhone: (phone: string) => set({ phone }),
-  setEmail: (email: string) => set({ email }),
-  setWhatsapp: (whatsapp: string) => set({ whatsapp }),
+  setName: (name) => set({ name }),
+  setGender: (gender) => set({ gender }),
+  setProfession: (profession) => set({ profession }),
+  setBio: (bio) => set({ bio }),
+  setPhone: (phone) => set({ phone }),
+  setEmail: (email) => set({ email }),
+  setWhatsapp: (whatsapp) => set({ whatsapp }),
 
-  setLinkOneUrl: (linkOneUrl: string) => set({ linkOneUrl }),
-  setLinkTwoUrl: (linkTwoUrl: string) => set({ linkTwoUrl }),
-  setLinkThreeUrl: (linkThreeUrl: string) => set({ linkThreeUrl }),
-  setLinkFourUrl: (linkFourUrl: string) => set({ linkFourUrl }),
-  setLinkFiveUrl: (linkFiveUrl: string) => set({ linkFiveUrl }),
+  setLinkOneUrl: (linkOneUrl) => set({ linkOneUrl }),
+  setLinkTwoUrl: (linkTwoUrl) => set({ linkTwoUrl }),
+  setLinkThreeUrl: (linkThreeUrl) => set({ linkThreeUrl }),
+  setLinkFourUrl: (linkFourUrl) => set({ linkFourUrl }),
+  setLinkFiveUrl: (linkFiveUrl) => set({ linkFiveUrl }),
 
-  setLinkOneTitle: (linkOneTitle: string) => set({ linkOneTitle }),
-  setLinkTwoTitle: (linkTwoTitle: string) => set({ linkTwoTitle }),
-  setLinkThreeTitle: (linkThreeTitle: string) => set({ linkThreeTitle }),
-  setLinkFourTitle: (linkFourTitle: string) => set({ linkFourTitle }),
-  setLinkFiveTitle: (linkFiveTitle: string) => set({ linkFiveTitle }),
+  setLinkOneTitle: (linkOneTitle) => set({ linkOneTitle }),
+  setLinkTwoTitle: (linkTwoTitle) => set({ linkTwoTitle }),
+  setLinkThreeTitle: (linkThreeTitle) => set({ linkThreeTitle }),
+  setLinkFourTitle: (linkFourTitle) => set({ linkFourTitle }),
+  setLinkFiveTitle: (linkFiveTitle) => set({ linkFiveTitle }),
 
   resetAccount: () => set(INITIAL_STATE),
 }));
